240d7afde606435e9ab8a038fb71011426477264
Craig McLeod <mcleod.craig@outlook.com>
2020-05-19 17:46:10 +0100

    Initial commit

---
+++ specs/fish_spec.rb
@@ -0,0 +1,13 @@
+require ("minitest/autorun")
+require ("minitest/reporters")
+
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative("../bear")
+require_relative("../fish")
+require_relative("../river")
+
+class FishTest < MiniTest::Test
+
+end
